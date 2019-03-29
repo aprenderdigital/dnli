9ddab5aeb41f4f1d5c857f249bad3374182edc1a
APRENDER.digital <admin@aprender.digital>
2019-03-29 09:23:39 -0300

    teste facebook

--- assets/js/redes.sociais.js
+++ assets/js/redes.sociais.js
@@ -1,11 +1,14 @@
 //Verifica se já curtiu a página
   FB.init({
-    appId  : 'APPID',
+    appId  : '1882232665401443',
     status : true, // check login status
     cookie : true, // enable cookies to allow the server to access the session
   });
  
-FB.api({ method: 'fql.query', query: 'SELECT uid FROM page_fan WHERE uid=UID AND page_id=PAGEID' },
+  var user_id = response.session.uid;
+
+FB.api({ method: 'fql.query', query: 
+         'SELECT uid FROM page_fan WHERE uid='+user_id+' AND page_id=1707310592820237' },
     function(result) {
         if (result.length){ 
             document.getElementById("textoCurtir").innerHTML = "Já curtiu";
