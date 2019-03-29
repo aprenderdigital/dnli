2812fae5806966c9001346f79cd4cca56490c563
APRENDER.digital <admin@aprender.digital>
2019-03-29 11:47:01 -0300

    teste facebook

--- assets/js/redes.sociais.js
+++ assets/js/redes.sociais.js
@@ -1,25 +1,26 @@
 //Verifica se já curtiu a página
-  FB.init({
+FB.init({
     appId  : '1882232665401443',
     status : true, // check login status
     cookie : true, // enable cookies to allow the server to access the session
-  });
+});
  
-  var user_id = 0;
+var user_id = 0;
+  
   FB.getLoginStatus(function(response) {
-    alert(response.authResponse.userID);
+    console.log("FB.getLoginStatus");
     user_id = response.authResponse.userID;
-  });
-alert (user_id);
-FB.api({ method: 'fql.query', query: 
-         'SELECT uid FROM page_fan WHERE uid='+user_id+' AND page_id=1707310592820237' },
+    FB.api({ method: 'fql.query', query: 
+    'SELECT uid FROM page_fan WHERE uid='+user_id+' AND page_id=1707310592820237' },
     function(result) {
+        console.log("FB.api");
         if (result.length){ 
             document.getElementById("textoCurtir").innerHTML = "Já curtiu";
         } 
- 
-        else { 
-            document.getElementById("textoCurtir").innerHTML = "Ainda não curtiu";
+       else { 
+        document.getElementById("textoCurtir").innerHTML = "Ainda não curtiu";
         }
     })
+  });
+alert (user_id);
 
